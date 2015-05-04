9f12c81ccfd95dbe21249a36c4bf98e206c55887
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-05-04 16:34:16 -0500

    DB migration and model fun

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+# Import modals
+from collection.models import Thing
+
+# Setup automatic slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = { 'slug': ('name',)}
+
 # Register your models here.
+admin.site.register(Thing, ThingAdmin)
