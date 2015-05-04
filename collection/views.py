e552c2183a010dc769e883354899cb27a3712f91
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-05-04 16:07:22 -0500

    Added var to controller(?) to pass into view

--- collection/views.py
+++ collection/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     # this is your new view
-    return render(request, 'index.html')
+
+    number = 6
+
+    return render(request, 'index.html', {
+        'number': number,
+    })
